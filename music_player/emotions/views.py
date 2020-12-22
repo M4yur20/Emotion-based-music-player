bafef12e17a3fccbf5984ccf0acf4eabc1927f46
M4yur20 <mayuragarwal929@gmail.com>
2020-12-22 19:04:53 +0530

    hogaya

--- music_player/emotions/views.py
+++ music_player/emotions/views.py
@@ -7,27 +7,22 @@ from keras.losses import categorical_crossentropy
 from keras.optimizers import Adam
 import cv2
 from .forms import *
+
 json_file = open(r'C:\Users\mishr\Downloads\model786.json', 'r')
 loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 loaded_model.load_weights(r'C:\Users\mishr\Downloads\model786.h5')
 loaded_model.compile(loss=categorical_crossentropy,
-                          optimizer=Adam(lr=0.001),
-                        metrics=['accuracy'])
-            
-face_classifier=cv2.CascadeClassifier(r'C:\Users\mishr\haarcascade_frontalface_alt.xml')
+                     optimizer=Adam(lr=0.001),
+                     metrics=['accuracy'])
+face_classifier = cv2.CascadeClassifier(r'C:\Users\mishr\haarcascade_frontalface_alt.xml')
+
 
 @require_POST()
 def predict(request):
-    form=PredictionForm(request.data,request.FILES)
+    form = PredictionForm(request.data, request.FILES)
     if form.is_valid():
-        ins=form.save(commit=False)
-        image=cv2.imread(ins.image.path,0) 
-        faces=face_classifier.detectMultiScale(image,1.3,5)
-        
-
-        
-
-    
-
+        ins = form.save(commit=False)
+        image = cv2.imread(ins.image.path, 0)
+        faces = face_classifier.detectMultiScale(image, 1.3, 5)
--- music_player/music_player/urls.py
+++ music_player/music_player/urls.py
@@ -13,15 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name="home"),
+    path('start-page/',views.startpage,name="startpage"),
     path('playlist/',include('playlist.urls')),
-    path('',views.home,name='home'),
     path('accounts/',include('accounts.urls')),
 
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
