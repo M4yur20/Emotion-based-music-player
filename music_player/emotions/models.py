d7c60a2e24d7d70c5f158060afca332fb9a4d6e5
M4yur20 <mayuragarwal929@gmail.com>
2020-12-22 18:28:17 +0530

    done

--- music_player/emotions/models.py
+++ music_player/emotions/models.py
@@ -4,8 +4,12 @@ from django.db import models
 # Create your models here.
 class Prediction(models.Model):
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
     image = models.ImageField()
     emotion = models.CHarField(max_length=30)
+=======
+    image = models.ImageField(upload_to='Prediction_Images')
+>>>>>>> Stashed changes
 =======
     image = models.ImageField(upload_to='Prediction_Images')
 >>>>>>> Stashed changes
