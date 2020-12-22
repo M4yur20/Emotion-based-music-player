0666ea97af56d50140a795ec1b2ae22393a608e3
Vinayak333-tech <mishravinayak839@gmail.com>
2020-12-22 17:57:16 +0530

    fourth

--- music_player/emotions/models.py
+++ music_player/emotions/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Prediction(models.Model):
+    image=models.ImageField()
--- music_player/playlist/models.py
+++ music_player/playlist/models.py
@@ -7,7 +7,7 @@ class Song(models.Model):
                (1, 'angry'),
                (2, 'sad'),
                (3, 'neutral'))
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     track = models.FileField()
     type = models.IntegerField(choices)
     name = models.CharField(max_length=30, default="Unknown", blank=True)
