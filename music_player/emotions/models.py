1b44b8df83ff82b68915e21e287f2c0ad0abb71c
M4yur20 <mayuragarwal929@gmail.com>
2020-12-22 18:27:07 +0530

    done

--- music_player/emotions/models.py
+++ music_player/emotions/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
 class Prediction(models.Model):
-    image=models.ImageField()
-    emotion=models.CHarField(max_length=30)
+    image = models.ImageField()
+    emotion = models.CHarField(max_length=30)
