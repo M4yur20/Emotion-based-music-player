e26401a588023f486e949879622f6c8046e38592
M4yur20 <mayuragarwal929@gmail.com>
2020-12-23 22:34:52 +0530

    dd

--- music_player/playlist/views.py
+++ music_player/playlist/views.py
@@ -5,6 +5,7 @@ from .models import Song
 from .forms import *
 from django.contrib import messages
 
+
 # Create your views here.
 
 
@@ -12,35 +13,35 @@ from django.contrib import messages
 def general(request):
     user = request.user
     songs = Song.objects.all().filter(user=user)
-    if len(songs)==0:
-        messages.info(request,f'Your playlist is empty.')
+    if len(songs) == 0:
+        messages.info(request, f'Your playlist is empty.')
     return render(request, 'playlist/general.html', {'songs': songs})
 
 
 def emotion(request, type):
     user = request.user
     songs = Song.objects.all().filter(user=user, type=type)
-    if len(songs)==0:
-        messages.info(request,f'Your playlist is empty.')
+    if len(songs) == 0:
+        messages.info(request, f'Your playlist is empty.')
     dic = {0: 'Happy',
            1: 'Angry',
            2: 'Sad',
            3: 'Neutral'}
-    return render(request, 'playlist/type.html', {'songs': songs, 'type': dic[type],'emotion':type})
+    return render(request, 'playlist/type.html', {'songs': songs, 'type': dic[type], 'emotion': type})
 
 
 @login_required(login_url='accounts:login')
-def song_upload(request,type):
+def song_upload(request, type):
     if request.method == "POST":
         form = SongUploadForm(request.POST, request.FILES)
         instance = form.save(commit=False)
         instance.user = request.user
-        instance.type=type
+        instance.type = type
         instance.save()
         return redirect('startpage')
     else:
         form = SongUploadForm()
-    return render(request, 'playlist/song_upload.html', {'form': form,'type':type})
+    return render(request, 'playlist/song_upload.html', {'form': form, 'type': type})
 
 
 @login_required(login_url='accounts:login')
@@ -50,19 +51,20 @@ def up_song(request):
         if form.is_valid():
             instance = form.save(commit=False)
             instance.user = request.user
-            typee=request.POST.get('playlist')
-            instance.type=typee
+            typee = request.POST.get('playlist')
+            instance.type = typee
             instance.save()
-            return redirect('startpage')
+            return redirect('playlist:general')
     else:
         form = SongUploadForm()
     return render(request, 'playlist/up-song.html', {'form': form})
 
 
-def delete(request,type,id):
-    song=Song.objects.get(pk=id)
+@login_required(login_url='accounts:login')
+def delete(request, type, id):
+    song = Song.objects.get(pk=id)
     song.delete()
-    if type==4:
+    if type == 4:
         return redirect('playlist:general')
     else:
-        return redirect('playlist:emotion',type=type)
+        return redirect('playlist:emotion', type=type)
