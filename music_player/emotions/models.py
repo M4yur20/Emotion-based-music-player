498d10e3dfb012a47db99aa8a65c8e81250f5a17
Vinayak333-tech <mishravinayak839@gmail.com>
2021-01-11 09:55:20 +0530

    m

--- music_player/emotions/models.py
+++ music_player/emotions/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Prediction(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     emotion = models.CharField(max_length=30)
     image = models.ImageField(upload_to='PredictionImages')
 
